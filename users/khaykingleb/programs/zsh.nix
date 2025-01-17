e677f09fc516f17e578a3c11f7fb0c737e0aae9a
Gleb Khaykin <khaykingleb@gmail.com>
2025-01-17 13:58:25 +0700

    feat(zsh): setup powerlevel10k

--- users/khaykingleb/programs/zsh.nix
+++ users/khaykingleb/programs/zsh.nix
@@ -1,17 +1,34 @@
 # Shell designed for interactive use
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   programs.zsh = {
     enable = true;
     syntaxHighlighting.enable = true;
     autosuggestion.enable = true;
 
+    shellAliases = {
+      vim = "nvim";
+    };
+
+    # Community-driven framework for managing the zsh configuration
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
     };
+
+    plugins = [
+      # Theme for Zsh
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
 
     # Custom ZSH settings
     initExtra = ''
+      # Source powerlevel10k
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
       # Cursor style
       echo -e -n "\x1b[\x35 q"                  # Use beam shape cursor on startup
       preexec() { echo -e -n "\x1b[\x35 q" ; }  # Use beam shape cursor for each new prompt
@@ -27,5 +44,12 @@
       # Undo
       bindkey "Ω" undo                 # Option + z: undo last edit
     '';
+  };
+
+  # Save Powerlevel10k config to the user's home directory
+  home.file = {
+    ".p10k.zsh" = {
+      source = ./config/p10k.zsh;
+    };
   };
 }
