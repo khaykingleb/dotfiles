0fc56eff6ede550d38ba7f207514a25ee65f7dea
Gleb Khaykin <khaykingleb@gmail.com>
2025-01-20 02:28:22 +0700

    chore: enable extra config for git

--- users/khaykingleb/programs/git.nix
+++ users/khaykingleb/programs/git.nix
@@ -5,8 +5,18 @@
 }: {
   programs.git = {
     enable = true;
-    lfs.enable = true;
     userName = "Gleb Khaykin";
     userEmail = "khaykingleb@gmail.com";
+
+    lfs.enable = true;
+
+    extraConfig = {
+      # Push to remote automatically if it's not set (won't need to run `git push --set-upstream origin <branch>` every time)
+      push.autoSetupRemote = true;
+      # Pull with rebase instead of merge
+      pull.rebase = true;
+      # Automatically setup merge branches when pulling
+      branch.autoSetupMerge = true;
+    };
   };
 }
