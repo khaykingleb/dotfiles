e39141fbafe179cf8083424ba3e80452e17c60e7
Gleb Khaykin <khaykingleb@gmail.com>
2025-01-21 22:14:30 +0700

    refactor: :hammer:

--- users/khaykingleb/programs/iterm2/default.nix
+++ users/khaykingleb/programs/iterm2/default.nix
@@ -1,16 +1,12 @@
 { pkgs, ... }: {
-  # Set up font to MesloLGS NF
+  # Save the preferences to a custom folder or URL:
+  # iTerm2 → Settings → General → Settings → Load settings from a custom folder or URL
+  #  → Specify a custom folder or URL → Save changes
+  # Then move this file here to com.googlecode.iterm2.plist
+
+  # If you set up iTerm2 from scratch, you need to take com.googlecode.iterm2.plist from the repo
+  # and save its contents to ~/Library/Preferences/iterm2/com.googlecode.iterm2.plist
   home.packages = with pkgs; [
     iterm2
   ];
-
-  home.file = {
-    # iTerm2 has an option to export the preferences to a custom location
-    # (iTerm2 → Settings → General → Settings → Load settings from a custom folder or URL
-    #  → Specify a custom folder or URL → Save changes)
-    # Then move this file here
-    "Library/Preferences/com.googlecode.iterm2.plist" = {
-      source = ./com.googlecode.iterm2.plist;
-    };
-  };
 }
