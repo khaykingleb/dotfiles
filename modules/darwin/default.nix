d8d844a08aed00b15feafec6c4a14ea85a956f80
Gleb Khaykin <khaykingleb@gmail.com>
2025-02-10 19:47:25 +0700

    refactor: :hammer:

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -8,11 +8,4 @@
 
   # Auto upgrade nix package and the daemon service.
   # services.nix-daemon.enable = true;
-
-  nix = {
-    settings = {
-      # Necessary for using flakes on this system
-      experimental-features = "nix-command flakes";
-    };
-  };
 }
