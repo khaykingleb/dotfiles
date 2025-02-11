35a75ab858fb24326db9649f1184b9b5a6cbba5d
Gleb Khaykin <khaykingleb@gmail.com>
2025-02-12 02:18:32 +0700

    style: :art:

--- users/khaykingleb/scripts/asdf.sh
+++ users/khaykingleb/scripts/asdf.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 
-plugins_to_add=(
-    "tflint"
-    "terraform-docs"
-    "pnpm"
-    "yarn"
+custom_plugins_to_add=(
     "supabase-cli https://github.com/gavinying/asdf-supabase-cli.git"
 )
-for plugin in "${plugins_to_add[@]}"; do
+for plugin in "${custom_plugins_to_add[@]}"; do
     asdf plugin-add $plugin
 done
 
 declare -A plugins_to_install=(
-    ["direnv"]="2.35.0"
-    ["pre-commit"]="4.1.0"
     ["python"]="3.12.9"
     ["poetry"]="2.0.1"
     ["rust"]="1.84.1"
     ["golang"]="1.23.5"
-    ["opentofu"]="1.9.0"
     ["terraform"]="1.5.7"
+    ["opentofu"]="1.9.0"
+    ["tflint"]="0.55.1"
+    ["terraform-docs"]="0.19.0"
     ["kubectl"]="1.32.1"
     ["awscli"]="2.23.3"
     ["nodejs"]="23.6.1"
+    ["pnpm"]="9.15.5"
+    ["yarn"]="1.22.22"
     ["supabase-cli"]="2.9.1"
+    ["direnv"]="2.35.0"
+    ["pre-commit"]="4.1.0"
 )
 for plugin in "${!plugins_to_install[@]}"; do
     asdf plugin-add $plugin
