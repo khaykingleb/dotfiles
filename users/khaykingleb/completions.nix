1abba1d4a3c61ee6f375d08aecf58aac9849ddf7
Gleb Khaykin <khaykingleb@gmail.com>
2025-02-25 22:44:02 +0700

    fix(asdf): completions didn't work before

--- users/khaykingleb/completions.nix
+++ users/khaykingleb/completions.nix
@@ -1,7 +1,4 @@
 { ... }: {
-  # NOTE:
-  # * https://docs.docker.com/engine/cli/completion/#zsh
-  # * https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
   home.activation.docker = ''
     docker_bin="/usr/local/bin/docker"
     if [ ! -d ~/.docker/completions ]; then
@@ -13,13 +10,20 @@
     fi
   '';
   programs.zsh.initExtra = ''
+    # >>> ASDF completions
+    # https://asdf-vm.com/guide/getting-started-legacy.html
+    FPATH="$HOME/.asdf/completions:$FPATH"
+    # <<< ASDF completions
+
     # >>> Docker completions
+    # https://docs.docker.com/engine/cli/completion/#zsh
     FPATH="$HOME/.docker/completions:$FPATH"
     autoload -Uz compinit
     compinit
     # <<< Docker completions
 
     # >>> Kubectl completions
+    # https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
     source <(kubectl completion zsh)
     # <<< Kubectl completions
   '';
