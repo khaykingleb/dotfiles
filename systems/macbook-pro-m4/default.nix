0849c166332a3d6f203d6b46842e4c1cdc1bb40f
Gleb Khaykin <khaykingleb@gmail.com>
2026-07-26 21:29:40 +0200

    refactor(home-manager): deduplicate Darwin configuration

--- systems/macbook-pro-m4/default.nix
+++ systems/macbook-pro-m4/default.nix
@@ -1,16 +1,9 @@
 # Handles system-level configuration for Macbook Pro M4 for personal use (macOS)
-{ user, ... }:
+{ ... }:
 {
-  imports = [
-    ../../modules/darwin
-    ./home-manager.nix
-  ];
+  imports = [ ../../modules/darwin ];
 
   my.isPersonal = true;
-
-  # Used for backwards compatibility
-  system.stateVersion = 5;
-  system.primaryUser = user;
 
   # networking.hostName = hostName;
 }
