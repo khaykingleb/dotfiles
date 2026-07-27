3915fda4cf4e4078e303bfb0273e5f1a7b1fd534
Gleb Khaykin <khaykingleb@gmail.com>
2026-07-27 02:13:10 +0200

    fix(asdf): initialize shims after path helper

--- users/shared/programs/asdf.nix
+++ users/shared/programs/asdf.nix
@@ -1,12 +1,12 @@
 # Runtime version manager
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
     asdf-vm
   ];
   programs.zsh = {
     # NOTE: https://asdf-vm.com/guide/getting-started.html
-    initContent = ''
+    initContent = lib.mkAfter ''
       # >>> ASDF
       . "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh"
       export PATH="$HOME/.asdf/shims:$PATH"
