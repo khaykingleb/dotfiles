395ecd27bbe2f756b167bbb9250d1a0be340c6dc
Gleb Khaykin <khaykingleb@gmail.com>
2026-08-15 19:45:16 +0200

    feat(kubernetes): add Krew plugins and completions

--- users/shared/programs/krew.nix
+++ users/shared/programs/krew.nix
@@ -1,5 +1,22 @@
-{ ... }:
+{ lib, pkgs, ... }:
+let
+  completionPlugins = [
+    "krew"
+    "neat"
+    "tree"
+    "who-can"
+  ];
+
+  mkCompletionAdapter =
+    plugin:
+    pkgs.writeShellScriptBin "kubectl_complete-${plugin}" ''
+      exec kubectl ${lib.escapeShellArg plugin} __complete "$@"
+    '';
+in
 {
+  # kubectl discovers plugin completers through executables named kubectl_complete-<plugin>.
+  home.packages = map mkCompletionAdapter completionPlugins;
+
   programs.zsh = {
     initContent = ''
       # >>> KREW
